8dbc10c939cdb534ccd4711205cf2caec54cc98f
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-07-22 18:56:34 -0300

    learned the redux toolkit method

--- sec18-redux/src/store/index.js
+++ sec18-redux/src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
@@ -14,7 +13,7 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            state.counter = state.counter + action.amount;
+            state.counter = state.counter + action.payload;
         },
         toggleCounter(state) {
             state.showCounter = !state.showCounter;
@@ -22,26 +21,9 @@ const counterSlice = createSlice({
     }
 });
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return { counter: state.counter + 1, showCounter: state.showCounter }
-    };
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
-    if (action.type === 'increase') {
-        return { counter: state.counter + action.amount, showCounter: state.showCounter }
-    }
-
-    if (action.type === 'decrement') {
-        return { counter: state.counter - 1, showCounter: state.showCounter }
-    };
-
-    if (action.type === 'togglecounter') {
-        return { counter: state.counter, showCounter: !state.showCounter }
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions;
 export default store;
